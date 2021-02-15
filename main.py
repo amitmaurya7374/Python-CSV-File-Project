bcda8aa3e7bf7a9dc39dea2d118d8f92380e31d5
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-15 09:11:02 +0530

    added some notes

--- main.py
+++ main.py
@@ -24,7 +24,14 @@ of lot row of data then we will use a pandas libary which is built for data anal
 import pandas as pd
 
 """Only thesee thing we have to do as compare to above code . :)"""
+"""
+Some Notes:
+In pandas we have DataFrames and Series 
+DataFrames => is actually a whole table 
+Series is equivalent to list .It is a kind a single column from a table
 
+"""
 data = pd.read_csv("weather_data.csv")
-print(data)
-print(data["temp"])  # this will give a temperature
+print(data) # this is a dataframe
+print(data["temp"])  # this will give a temperature, this is a Series
+
