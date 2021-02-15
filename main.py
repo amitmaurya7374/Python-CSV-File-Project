7340efe7fe11f9177ced1bd169c002e00a9977f8
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-15 10:16:05 +0530

    Some basics of pandas library working with rows and columns

--- main.py
+++ main.py
@@ -32,6 +32,72 @@ Series is equivalent to list .It is a kind a single column from a table
 
 """
 data = pd.read_csv("weather_data.csv")
-print(data) # this is a dataframe
+print(data)  # this is a dataframe
 print(data["temp"])  # this will give a temperature, this is a Series
 
+"""COnverting a dataframe into a dictionary"""
+data_dict = data.to_dict()
+print(data_dict)
+
+"""converting a series into a list"""
+data_list = data["temp"].to_list()
+print(data_list)  # give a temperature list
+
+# way to long
+# average = 0
+# total = 0
+# for temp in data_list:
+#     print(temp)
+#     total += temp
+#     average = total / len(data_list)
+# print(f"average: {int(average)}")
+
+
+"""We have computations method in pandas"""
+average_temp = data["temp"].mean()
+print(average_temp)
+
+# getting a maximun value from a series
+max_temp = data["temp"].max()
+print(max_temp)
+
+# Gettting a  Data in Columns
+
+condition = data["condition"]  # one way to use
+print(condition)
+
+condition_2 = data.condition  # second way to access column by dot operation
+print(condition_2)
+
+# Getting hold of entire row
+"""Syntax : whole_table[column_that_i_want_to_search_through == "condition"]
+for eg:
+    data[data["conditon"] == "Rain"]
+"""
+first_row = data[data.day == "Monday"]
+print(first_row)
+
+condition_row = data[data["condition"] == "Rain"]
+print(condition_row)
+
+# Getting a entire row which has max temperature
+max_temp_row = data[data["temp"] == data["temp"].max()]
+print(max_temp_row)
+
+# Getting a single value from a entire row
+monday = data[data.day == "Monday"]
+mon_condition = monday.condition
+mon_temp = monday.temp
+temp_in_f = ((mon_temp * 9 / 5) + 32)
+print(f"temp in f :{temp_in_f}")
+print(mon_condition)
+
+# creating a data frame from a scratch
+score_dict = {
+    "name": ["Amit", "Luci", "Imaginary_friend"],
+    "score": [78, 666, 100]
+}
+score_data_table = pd.DataFrame(score_dict)
+print(score_data_table)
+# we can also convert this into a csv file
+score_data_table.to_csv("score_data.csv")
