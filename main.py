dc189fa987a2b39908d1b1e0c3030b1b834f667e
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-15 08:28:39 +0530

    learned how to use panda

--- main.py
+++ main.py
@@ -8,16 +8,23 @@ csv stands for comma seprated value.
 #     data = file.readlines()
 # print(data)
 
-import csv
-
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        if row[1] != "temp":  # excludimg a temp label
-            temperature.append(int(row[1]))
-print(temperature)
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":  # excludimg a temp label
+#             temperature.append(int(row[1]))
+# print(temperature)
 
 """The above code  is only good for small row of data . what if we have lots 
 of lot row of data then we will use a pandas libary which is built for data analysis.
 """
+import pandas as pd
+
+"""Only thesee thing we have to do as compare to above code . :)"""
+
+data = pd.read_csv("weather_data.csv")
+print(data)
+print(data["temp"])  # this will give a temperature
